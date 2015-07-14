55aa33ad2dfe7b2844cdf69ed0043506279069e6
mjjngd <mjjngd@163.com>
2015-07-15 00:05:52 +0800

    7-14

---
+++ LeetcodeJava/src/com/leetcode/java/Leetcode1.java
@@ -0,0 +1,10 @@
+package com.leetcode.java;
+
+/**
+ * Created by Estelle on 2015/7/15.
+ */
+public class Leetcode1 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
